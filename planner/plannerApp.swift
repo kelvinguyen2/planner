47c18251534cdd37cc4583d109cffdcbce34600a
kelvin <kelvinguyen2@gmail.com>
2025-01-17 08:07:10 -0800

    Initial Commit

---
+++ planner/plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  plannerApp.swift
+//  planner
+//
+//  Created by Kelvin Nguyen on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plannerTests/plannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  plannerTests.swift
+//  plannerTests
+//
+//  Created by Kelvin Nguyen on 1/17/25.
+//
+
+import Testing
+@testable import planner
+
+struct plannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
